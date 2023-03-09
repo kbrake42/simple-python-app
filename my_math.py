7e35ef16efbb8a82b3556979a853b1165db8738c
Ken <kbrake42@gmail.com>
2023-03-09 17:45:30 -0500

    Initial checkin.

---
+++ my_math.py
@@ -0,0 +1,6 @@
+
+def my_add(x:int,y:int) -> int:
+    return x + y
+
+print(my_add(2,3))
+
---
+++ test_math.py
@@ -0,0 +1,9 @@
+from my_math import my_add
+
+def test_my_add():
+    x = 2
+    y = 3
+
+    actual = my_add(x,y)
+
+    assert actual == 5
