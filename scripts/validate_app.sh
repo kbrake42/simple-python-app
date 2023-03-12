c44278d2278fa41bf80635b374a50873eb1c3fff
Ken <kbrake42@gmail.com>
2023-03-11 21:26:13 -0500

    Update to validate_app

--- scripts/validate_app.sh
+++ scripts/validate_app.sh
@@ -3,10 +3,11 @@
 # Define a variable
 result=$(python3 /tmp/python_app/src/my_math.py)
 
-if [ $result -eq 4 ]
+if [ $result -eq 5 ]
 then
+    echo ********validation passed**************
     exit 0
 else
+    echo ********validation failed**************
     exit 1
 fi
-
